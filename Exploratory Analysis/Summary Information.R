fc6075fc238f35aaef7ea6ef5c45223bcec38808
Sai Raksha Rayala <srayala2@uw.edu>
2021-11-22 22:41:09 -0800

    update

--- Exploratory Analysis/Summary Information.R
+++ Exploratory Analysis/Summary Information.R
@@ -43,13 +43,13 @@ View(state_vaccines)
 highest_state_fully_vaccinated <- state_vaccines %>%
   filter(date == max(date)) %>%
   filter(people_fully_vaccinated_per_hundred == max(people_fully_vaccinated_per_hundred, na.rm = TRUE)) %>%
-  pull(location)
+  pull(location, people_fully_vaccinated_per_hundred)
 
 #Which state or territory has the lowest fully vaccinated rate?
 lowest_state_fully_vaccinated <- state_vaccines %>%
   filter(date == max(date)) %>%
   filter(people_fully_vaccinated_per_hundred == min(people_fully_vaccinated_per_hundred, na.rm = TRUE)) %>%
-  pull(location)
+  pull(location, people_fully_vaccinated_per_hundred)
 
 #What are the current fully vaccinated rates for all the states?
 regional_fully_vaccinated <- state_vaccines %>%
@@ -62,10 +62,16 @@ print(regional_fully_vaccinated)
 highest_dose_usage <- state_vaccines %>%
   filter(date == max(date)) %>%
   filter(share_doses_used == max(share_doses_used, na.rm = TRUE)) %>%
-  pull(location)
+  pull(location, share_doses_used)
 
 #Which state is currently the least effective at using its doses?
 lowest_dose_usage <- state_vaccines %>%
   filter(date == max(date)) %>%
   filter(share_doses_used == min(share_doses_used, na.rm = TRUE)) %>%
+  pull(location, share_doses_used)
+
+#How many regions have used less than half of their population fully vaccinated?
+less_than_half_pop <- regional_vaccines %>%
+  filter(date == max(date)) %>%
+  filter(people_fully_vaccinated_per_hundred <= 0.5) %>%
   pull(location)
