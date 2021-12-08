aa4dfaedacf872a10a6b5360d4dcac14d59f4deb
thenavman10 <92080479+thenavman10@users.noreply.github.com>
2021-12-07 17:38:17 -0800

    Update BarChart.R

--- Exploratory Analysis/BarChart.R
+++ Exploratory Analysis/BarChart.R
@@ -51,9 +51,9 @@ arrange(us_months_vaccines, date)
 
 vaccines_bar_chart <- ggplot(us_months_vaccines) +
   geom_col(mapping = aes(x = date, y = people_fully_vaccinated)) + coord_flip() +
-labs(
-  title = "Fully Vaccinated People in US by Month", # plot title
-  x = "Number of People Fully Vaccinated", # x-axis label
-  y = "2021 Ending Date (with data collected) of each Month", # y-axis label
-)
-    
+  labs(
+    title = "Fully Vaccinated People in US by Month", # plot title
+    x = "Number of People Fully Vaccinated", # x-axis label
+    y = "2021 Ending Date (with data collected) of each Month", # y-axis label
+  )
+
